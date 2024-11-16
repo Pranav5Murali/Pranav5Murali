7242205a0b68be36553b5a6d05ffb12a2e5a9bdf
Pranav5Murali <154644683+Pranav5Murali@users.noreply.github.com>
2024-11-16 09:51:35 +0530

    Update copy_file.sh

--- copy_file.sh
+++ copy_file.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Hardcoded variables
-DEST_USER="karthik"
-DEST_IP="192.168.1.105"
-DEST_PASSWORD="$1"
-SCRIPT_NAME="$2"
+DEST_USER="$1"
+DEST_IP="$2"
+DEST_PASSWORD="$3"
+SCRIPT_NAME="$4"
 
+if [ -z "$DEST_USER" ] || [ -z "$DEST_IP" ] || [ -z "$DEST_PASSWORD" ] || [ -z "$SCRIPT_NAME" ]; then
+    echo "Usage: ./copy_file.sh <username> <ip_address> <password> <script_name>"
+    exit 1
+fi
 
-# Define the destination path on the remote server
 DEST_PATH="/home/$DEST_USER/git_target"
 
-echo "Copying Python script to $DEST_USER@$DEST_IP..."
-
-# Copy the Python script to the remote server using sshpass and scp
 sshpass -p "$DEST_PASSWORD" scp -o StrictHostKeyChecking=no "./$SCRIPT_NAME" "$DEST_USER@$DEST_IP:$DEST_PATH"
-COPY_STATUS=$?
-
-# Check if the script was copied successfully
-if [ "$COPY_STATUS" -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo "Python script copied successfully"
 else
     echo "Error occurred while copying the script"
